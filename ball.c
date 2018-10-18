413b723c9d8ba230783a64987cad971edcf9a5ba
Sean <spl48@uclive.ac.nz>
2018-10-19 10:59:42 +1300

    Forgot to commit ball module

---
+++ ball.c
@@ -0,0 +1,62 @@
+/**
+ @file ball.c
+ @authors Sean Plane spl48, John Kim
+ @date 18/10/2018
+ **/
+
+ #include "ball.h"
+ #include "boing.h"
+ #include "paddle.h"
+ #include "tinygl.h"
+
+int update_ball_speed(int ball_speed)
+{
+    if (ball_speed > 1) {
+        ball_speed--;
+    }
+    return ball_speed;
+}
+
+boing_state_t rebound_off_paddle(boing_state_t ball, tinygl_point_t hops[])
+{
+    boing_dir_t newdir[] = {DIR_N, DIR_NW, DIR_W, DIR_SW,
+                                DIR_S, DIR_SE, DIR_E, DIR_NE};
+    ball.pos.x -= 2 * hops[ball.dir].x;
+    ball.dir = newdir[ball.dir];
+    return ball;
+}
+
+boing_state_t rebound_off_side(boing_state_t ball, tinygl_point_t hops[])
+{
+    boing_dir_t newdir[] = {DIR_S, DIR_SE, DIR_E, DIR_NE,
+                                    DIR_N, DIR_NW, DIR_W, DIR_SW};
+    ball.pos.y -= 2 * hops[ball.dir].y;
+    ball.dir = newdir[ball.dir];
+    return ball;
+}
+
+boing_state_t move_ball(boing_state_t ball)
+ {
+    tinygl_point_t hops[] = {{0, 1}, {1, 1}, {1, 0}, {1, -1},
+                             {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+
+    ball.pos.x += hops[ball.dir].x;
+    ball.pos.y += hops[ball.dir].y;
+    return ball;
+ }
+
+boing_state_t update_ball (boing_state_t ball, paddle_t player1, paddle_t player2)
+{
+    tinygl_point_t hops[] = {{0, 1}, {1, 1}, {1, 0}, {1, -1},
+                             {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+    if(ball_collides(ball, player1) == 1 || ball_collides(ball, player2)){
+        ball = rebound_off_paddle(ball, hops);
+    }
+
+    if (ball.pos.y > TINYGL_HEIGHT - 1 || ball.pos.y < 0)
+    {
+        ball = rebound_off_side(ball, hops);
+    }
+
+    return ball;
+}
---
+++ ball.h
@@ -0,0 +1,24 @@
+/**
+ @file ball.h
+ @authors Sean Plane spl48, John Kim
+ @date 18/10/2018
+ **/
+
+#ifndef BALL_H
+#define BALL_H
+
+ #include "boing.h"
+ #include "paddle.h"
+ #include "tinygl.h"
+
+int update_ball_speed(int ball_speed);
+
+boing_state_t rebound_off_paddle(boing_state_t ball,tinygl_point_t hops[]);
+
+boing_state_t rebound_off_side(boing_state_t ball, tinygl_point_t hops[]);
+
+boing_state_t move_ball(boing_state_t ball);
+
+boing_state_t update_ball (boing_state_t ball, paddle_t player1, paddle_t player2);
+
+#endif
